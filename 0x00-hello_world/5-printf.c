a7e24fe29fb8cc8142b9dfc3c5e41fcaca3ae132
beckyshaz <sharonsakah8@gmail.com>
2023-02-20 17:42:03 -0800

    printf program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,8 @@
-#!/bin/bash 
-printf("with proper grammer, but the outcome is a piece of art,\n");
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
+
