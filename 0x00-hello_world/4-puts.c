f3130384a6c7d56189ab2e9978c5eef74b36cc2a
beckyshaz <sharonsakah8@gmail.com>
2023-02-21 08:28:32 -0800

    puts it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-	char Mystr[] = ""Programming is like building a multilingual puzzle";
-	puts(Mystr);
+	puts(""programming is like building a multilingual puzzle);
 	return (0);
 
 
