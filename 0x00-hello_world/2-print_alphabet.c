99e49fa9228831e6870ad8d29ffd688bee06a046
beckyshaz <sharonsakah8@gmail.com>
2023-02-25 16:42:27 -0800

    a-z

---
+++ 0x00-hello_world/2-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ * print alphabet a-z
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+
+	{
+		putchar(ch);
+		ch++;
+		putchar('\n');
+
+	}
+
+
+
+
+
+
+
+
+
+return (0);
+
+}
