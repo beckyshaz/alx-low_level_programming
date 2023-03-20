aac6561d3d36c8d20d4ff546f6d3a75d7244d783
beckyshaz <sharonsakah8@gmail.com>
2023-03-20 16:13:27 -0700

    combination of two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return:0 (success)
+ *possible combinations of two digits
+ */
+int main(void)
+{
+	int x;
+	int y;
+	for (x = '0'; x <= '8'; x++)
+	{
+		for (y = '1'; y <= '9'; y++)
+		{
+			if (x < y && x != y) 
+		{
+			putchar(x);
+			putchar(y);
+			if (x + y != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}	
+		}	
+	}	
+
+		}
+	{
+		putchar('\n');
+		
+	}
+	return (0);
+	
+		
+}
+
