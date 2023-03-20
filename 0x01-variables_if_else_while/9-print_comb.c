f6119cdfcd7253a01fbb231b25d448b1711d62b1
beckyshaz <sharonsakah8@gmail.com>
2023-03-20 10:37:03 -0700

    0-9 separeted with space and commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,27 +7,27 @@
 int main(void)
 {
 	int x;
-	
-	 
-	for( x = '0'; x <= '9'; x++)
+
+
+	for (x = '0'; x <= '9'; x++)
 	{
 
-		
+
 		putchar(x);
-	
-		if(x != '9')
-		
-		
+
+		if (x != '9')
+
+
 		putchar(',');
 		putchar(' ');
-			 
-	
+
+
 	}
 
 	{
-		 
-		putchar('\n');	
+
+		putchar('\n');
 	}
-	    
+
 	return (0);
 }
