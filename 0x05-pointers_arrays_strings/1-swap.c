a2bdce8aacdf2df309d8abcc487e2eb1e2fcd7da
beckyshaz <sharonsakah8@gmail.com>
2023-03-27 19:20:49 -0700

    rectified

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,5 +7,9 @@
  */
 void swap_int(int *a, int *b)
 {
+	int c;
+
+	c = *a;
 	*a = *b;
+	*b = c;
 }
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,10 +2,10 @@
 /**
  * _strlen -  function that returns the length of a string
  *@s: string being returned
- *return: string
+ *return:*s  the length of a string
  */
 int _strlen(char *s)
 {
-	
+
 	return (*s);
 }
