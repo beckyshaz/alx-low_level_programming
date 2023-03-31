52d7c09428dd0d1a4e57cec5b9c1e225440141e0
beckyshaz <sharonsakah8@gmail.com>
2023-03-31 13:06:58 -0700

    function that concatenates n characters of a strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strncat -  function that concatenates n strings
+ *@dest: where concatenated string is to be printed
+ *@src: where the concatenated string is from
+ *Return: pointer to the resulting string
+ *@n: number of characters to be concatenated
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		destlen++;
+	for (i = 0; src[i] != '\0'; i++)
+		srclen++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[destlen + 1] = src[i];
+
+	return (dest);
+}
