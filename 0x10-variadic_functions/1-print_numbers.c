165baa12adca64b08dca3cec81392c1c8dc72051
beckyshaz <sharonsakah8@gmail.comgit config user.name beckyshazgit config --global user.email sharonsakah8@gmail.com>
2023-07-20 15:16:58 -0700

    function that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ *print_numbers - is the name of the function that print the numbers passed
+ *@separator : is the string to be printed between numbers
+ *@n : is the number of integers passed to the function
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list(num);
+	unsigned int i;
+
+	va_start(num, n);
+	for (i = 0; i < n ; i++)
+	{
+		printf("%d", va_arg(num, int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(num);
+}
