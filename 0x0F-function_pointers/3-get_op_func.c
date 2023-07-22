986e083aca02a1ca36d8a2f3e2ab82448e09d31a
beckyshaz <sharonsakah8@gmail.comgit config user.name beckyshazgit config --global user.email sharonsakah8@gmail.com>
2023-07-22 09:33:52 -0700

    calc program

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+/**
+ *get_op_func -  returns a pointer to the function that corresponds to the operator
+ *@s : is the operator passed as argument to the program
+ *
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+    while (i < 5)
+    {
+	    if (*s == *ops[i].op)
+		    return (*ops[i].f);
+	    i++;
+    }
+    return 0;
+
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+/**
+ *main - Entry point
+ *@argc
+ *@argv 
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int result;
+	char s;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	s = argv[2][0];
+	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
+	{
+		printf ("Error\n");
+		exit (99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	result = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", result);
+
+	return 0;
+}
