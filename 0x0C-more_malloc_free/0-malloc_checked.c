f7d5ec4e0236c4a5a1620c444c4653a12ab19dc2
beckyshaz <sharonsakah8@gmail.com>
2023-08-10 07:40:03 -0700

    creating space in memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked -  function that allocates memory using malloc
+ *@b: What memory is being allocated for
+ *Return: void
+ *Otherwise NULL
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptrmalloc;
+
+	ptrmalloc = malloc(sizeof(unsigned int) * b);
+	if (ptrmalloc == NULL)
+	{
+		exit(98);
+	}
+	return (ptrmalloc);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+more malloc free is a project about memory allocatio in c and all tasks pertaining to the said topic
