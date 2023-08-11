4052d9a62cddfb4f27f65b499b0ec9973d0a80a1
beckyshaz <sharonsakah8@gmail.com>
2023-08-11 09:22:25 -0700

    memory reallocation using malloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_realloc - function to reallocate memory created by malloc
+ *@ptr: pointer created by malloc
+ *@old_size: size of memory created by malloc
+ *@new_size: size to now be used to allocate memory
+ *Return: void
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *pr;
+	unsigned int i;
+	char *oldp = ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	else if (ptr == NULL)
+		pr = malloc(new_size);
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	pr = malloc(new_size);
+	if (pr == NULL)
+		return (NULL);
+	if (new_size > old_size)
+	{
+		new_size = old_size;
+
+	}
+	for (i = 0; i < new_size; i++)
+		pr[i] = oldp[i];
+	free(ptr);
+	return (pr);
+}
