3220e3894dd40b55755c34b0136236b941945439
beckyshaz <sharonsakah8@gmail.com>
2023-08-14 15:56:00 -0700

    typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ *dog_t - Typedef for struct dog
+ *new_dog - Function that creates a new dog
+ *@name: Name of dog
+ *@age: Age of dog
+ *@owner: Owner of dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,14 +11,13 @@
 /**
  *dog_t - Typedef for struct dog
  */ 
-typedef struct dog dog_t;
+typedef struct dog 
 
-struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
