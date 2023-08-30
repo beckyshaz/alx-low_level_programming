5e24e29b47ff482ec3f948b672dc991b16f6ee2f
beckyshaz <sharonsakah8@gmail.com>
2023-08-30 16:17:04 -0700

    print opscode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,14 +2,38 @@
 #include <stdlib.h>
 
 /**
- *main - entry point
+ *main - entry point of opcode
  *@argc: number of arguments
  *@argv: pointer to array of arguments
- *Retutn 0 (Success)
+ *Return: 0 (Success)
  */
-int main(int  argc, *argv[])
+int main(int argc, char *argv[])
 {
+	int byts, i;
+	char *a;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	byts = atoi(argv[1]);
 
+	if (byts < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	a = (char *)main;
 
+	for (i = 0; i < byts; i++)
+	{
+		if (i == byts - 1)
+		{
+			printf("%02hhx\n", a[i]);
+			break;
+		}
+		printf("%02hhx ", a[i]);
+	}
+	return (0);
 }
