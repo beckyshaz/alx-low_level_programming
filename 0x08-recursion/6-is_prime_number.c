6961e46d42b5455edfa01c948c7ecbcaf3d9d445
beckyshaz <sharonsakah8@gmail.com>
2023-08-30 15:37:51 -0700

    if its prime or not

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,5 @@
 #include "main.h"
-
-/**
- *support_prime - function that supports is_prime_number
- *@number: Input
- *@i: divisor number
- *Return: 1 (true)
- *Otherwise 0
- */
-int support_prime(int number, int i)
-{
-	if (number % i == 0)
-	{
-		return (0);
-	}
-	else if (number == 2)
-	{
-		return (1);
-	}
-	else
-	{
-		return (support_prime(number, i + 1));
-	}
-}
+int support_prime(int n, int i);
 
 /**
  *is_prime_number - function that checks for prime number
@@ -37,8 +15,22 @@ int is_prime_number(int n)
 	}
 	else
 	{
-		return (support_prime(n, 0));
+		return (support_prime(n, n - 1));
 	}
+}
+/**
+ *support_prime - recursively checks if a number is prime or not
+ *@n: number to be checked
+ *@i: index of number
+ *Return: 1 if prime else 0
+ */
+int support_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (support_prime(n, i - 1));
 }
 
 
