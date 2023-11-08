09344cfc17303421d723599352652b47d671a590
beckyshaz <sharonsakah8@gmail.com>
2023-11-08 02:43:57 -0800

    copying n number of string from sorce to dest

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,15 +9,13 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int destlen = 0;
-	int srclen = 0;
 	int i;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		destlen++;
-	for (i = 0; src[i] != '\0'; i++)
-		srclen++;
-	for (i = 0; i < n && n <= srclen; i++)
-		dest[destlen + 1] = src[i];
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[destlen + i] = src[i];
+	dest[destlen + i] = '\0';
 
 	return (dest);
 }
