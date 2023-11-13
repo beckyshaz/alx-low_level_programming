2e01e3952c30edf59a529928f17b3c09adbb6733
beckyshaz <sharonsakah8@gmail.com>
2023-11-13 07:44:36 -0800

    function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strchr - function that allocates a char in a string
+ *@s: string to allocate char from
+ *@c: character to be allocated in the string
+ *Return: pointer to the first occurance of char else Null
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		if (s[index] == c)
+			break;
+		return (s + index);
+	}
+	return (NULL);
+}
