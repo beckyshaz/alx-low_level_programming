9c41b9df101ad0ab63fd11ef766dd01cced46dcb
beckyshaz <sharonsakah8@gmail.com>
2023-11-27 04:12:05 -0800

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ int _strlen(char *str)
  *@src: pointer to where the string is copied from
  *Return: pointer to dest
  */
-char *_strcpy(char*dest, char*src)
+char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
 	int i;
