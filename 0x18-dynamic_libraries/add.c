6821f44182ccba8440db0d288db2ffc5af429fb3
beckyshaz <sharonsakah8@gmail.com>
2024-01-29 07:40:39 -0800

    c libraries tat can be used in python

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,51 @@
+/**
+ *add - returns the sum of a and b
+ *@a: First number to be entered
+ *@b: Second number to be entered
+ *Return: addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *sub - returns the difference of a and b
+ *Return: a minus b
+ *@a: First number
+ *@b: Second number
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *mul - returns the product of a and b
+ *Return: a multiplied by b
+ *@a: first number
+ *@b: second number
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *div - returns the result of the division of a by b
+ *Return: a divided by b
+ *@a: first number to be input
+ *@b: second number to be input
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *mod - returns the remainder of the division of a by b
+ *Return: a modulo b
+ *@a: first number to be input
+ *@b: second number to be input
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
